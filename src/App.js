e2581b36372f03c423f53c0bbeb6e832bf5e6751
RinuZz <rinujasi32@gmail.com>
2023-11-21 10:05:06 +0530

    TodoApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [userInput, setUserInput] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSave = ()=>{
+    const obj={
+      title: userInput,
+      done:false
+    }
+
+    const clone = [...todos,obj]
+    setTodos(clone)
+    setUserInput('')
+  }
+
+  const handleRemove = (index)=>{
+    const clone=[...todos]
+    clone.splice(index,1)
+    setTodos(clone)
+  }
+
+  const handleCheck =(index=>{
+    const clone=[...todos]
+    clone[index].done = ! clone [index].done
+    setTodos(clone)
+  })
+
+  const handleKeyDown =(e)=>{
+    if(e.key==="Enter"){
+      handleSave()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <h1 className="todo-title">My Todos</h1>
+      <div className="add-area">
+        <input type="text" value={userInput} onChange={(e) => { setUserInput(e.target.value) }} onKeyDown={handleKeyDown} />
+        <button onClick={handleSave} >Save</button>
+      </div>
+      <ul className="todo-list">
+        {
+          todos.map((x, index) => {
+            return (
+              <li key={index}>
+                <div>
+                  <input type="checkbox" name="" id="" onClick={()=>{
+                    handleCheck(index)
+                  }} />
+                    {
+                      x.done ? <s>{x.title}</s>:x.title
+                    }
+                  </div>
+                <button onClick={()=>{handleRemove(index)}}>Remove</button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
+
   );
 }
 
